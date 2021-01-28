1a6d6d93b490e76a5708db0dda03ae42b4f31cf2
RomanovIgnat <65200927+RomanovIgnat@users.noreply.github.com>
2021-01-28 04:43:57 +0300

    Update SimpleAsyncrhonousEchoServer.go

--- SimpleAsyncrhonousEchoServer.go
+++ SimpleAsyncrhonousEchoServer.go
@@ -18,26 +18,26 @@ func echo(conn net.Conn) {
 
     if _, err := io.Copy(conn, conn); err != nil {
     	log.Fatalln("Unable to read/write data")
-	}
+    }
 }
 
 func main() {
     listener, err := net.Listen("tcp", "0.0.0.0:20080")
     if err != nil {
     	log.Fatalln("Unable to bind the port")
-	}
+    }
 
-	log.Println("Listening on 0.0.0.0:20080")
+    log.Println("Listening on 0.0.0.0:20080")
 
     for {
     	conn, err := listener.Accept()
     	log.Println("Received connection")
     	if err != nil {
-    		log.Fatalln("Unable to accept connection")
-		}
-
-		go echo(conn)
+    	    log.Fatalln("Unable to accept connection")
 	}
+
+	go echo(conn)
+    }
 
     return
 }
